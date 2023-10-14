bd2fbea5e89c2aeb64d40845f4c19af3a41ec934
Jérémie Lumbroso <lumbroso@seas.upenn.edu>
2023-10-14 17:32:58 -0400

    v0.3.0, refactored `invites.py`

--- slackish/invites.py
+++ slackish/invites.py
@@ -1,108 +1,65 @@
-def list_invites_history(session):
-    def lih_aux(cursor=None):
-        data = {
-            "token": session.auth_token,
-            "type": "accepted",
-            "sort_by": "date_create",
-            "sort_dir": "DESC",
-            # "_x_reason": "fetch_invites_history_admin",
-            # "_x_mode": "online",
-        }
-        if cursor is not None:
-            data["cursor"] = cursor
+def fetch_from_slack_api(session, endpoint, data, cursor=None):
+    """Fetch data from Slack API."""
+    if cursor:
+        data["cursor"] = cursor
 
-        response = session.post(
-            url="https://{}/api/users.admin.fetchInvitesHistory".format(
-                session.workspace
-            ),
-            params={
-                "fp": "2d",
-                "slack_route": session.ids.get("team_id"),
-                # "_x_id": "74293f22-1697225176.572",
-                # "_x_version_ts": "noversion",
-            },
-            headers={
-                "Authority": session.workspace,
-                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
-            },
-            data=data,
-        )
-        if response.status_code != 200:
-            raise Exception("Could not list invites history: " + response.text)
+    response = session.post(
+        url=f"https://{session.workspace}/api/{endpoint}",
+        params={
+            "fp": "2d",
+            "slack_route": session.ids.get("team_id"),
+        },
+        headers={
+            "Authority": session.workspace,
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+        },
+        data=data,
+    )
 
-        result = response.json()
+    if response.status_code != 200:
+        raise Exception(f"Could not fetch data from {endpoint}: " + response.text)
 
-        return result
+    return response.json()
 
-    invites = []
+
+def list_data_using_endpoint(session, endpoint, key_in_response, initial_data):
+    """Generic function to list data from Slack using a specified API endpoint."""
+    all_data = []
     cursor = None
+
     while True:
-        result = lih_aux(cursor=cursor)
+        result = fetch_from_slack_api(session, endpoint, initial_data, cursor)
+
         if not result["ok"]:
-            raise Exception("Could not list invites history: " + result["error"])
+            raise Exception(f"Could not fetch data from {endpoint}: " + result["error"])
 
-        # Add the current page of results to the list
-        invites += result["invites"]
+        all_data.extend(result[key_in_response])
 
-        # If there is a cursor, fetch the next page
-        if result.get("next_cursor", None) is not None:
-            cursor = result["next_cursor"]
-        else:
+        cursor = result.get("next_cursor")
+        if not cursor:
             break
 
-    return invites
+    return all_data
+
+
+def list_invites_history(session):
+    data = {
+        "token": session.auth_token,
+        "type": "accepted",
+        "sort_by": "date_create",
+        "sort_dir": "DESC",
+    }
+    return list_data_using_endpoint(
+        session, "users.admin.fetchInvitesHistory", "invites", data
+    )
 
 
 def list_invite_requests(session):
-    def lih_aux(cursor=None):
-        data = {
-            "token": session.auth_token,
-            # "type": "accepted",
-            "sort_by": "date_create",
-            "sort_dir": "DESC",
-            # "_x_reason": "fetch_invites_history_admin",
-            # "_x_mode": "online",
-        }
-        if cursor is not None:
-            data["cursor"] = cursor
-
-        response = session.post(
-            url="https://{}/api/users.admin.fetchInviteRequests".format(
-                session.workspace
-            ),
-            params={
-                "fp": "2d",
-                "slack_route": session.ids.get("team_id"),
-                # "_x_id": "74293f22-1697225176.572",
-                # "_x_version_ts": "noversion",
-            },
-            headers={
-                "Authority": session.workspace,
-                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
-            },
-            data=data,
-        )
-        if response.status_code != 200:
-            raise Exception("Could not list invites history: " + response.text)
-
-        result = response.json()
-
-        return result
-
-    invites = []
-    cursor = None
-    while True:
-        result = lih_aux(cursor=cursor)
-        if not result["ok"]:
-            raise Exception("Could not list invites history: " + result["error"])
-
-        # Add the current page of results to the list
-        invites += result["requests"]
-
-        # If there is a cursor, fetch the next page
-        if result.get("next_cursor", None) is not None:
-            cursor = result["next_cursor"]
-        else:
-            break
-
-    return invites
+    data = {
+        "token": session.auth_token,
+        "sort_by": "date_create",
+        "sort_dir": "DESC",
+    }
+    return list_data_using_endpoint(
+        session, "users.admin.fetchInviteRequests", "requests", data
+    )
